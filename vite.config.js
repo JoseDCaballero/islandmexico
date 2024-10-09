cf6e1d912ffc93f5514950e2509e84256b3b917f
JoseDCaballero <22090601@alumno.utmetropolitana.edu.mx>
2024-10-09 08:58:41 -0600

    hikjhk

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
             },
         }),
     ],
-    base: 'https://knight28.me/islandmexico/',
+    base: 'https://knight28.me/islandmexico',
     resolve: {
         alias: {
             ziggy: 'vendor/tightenco/ziggy/dist',
